f953324f49bba39c221e2c23507e1d9266f47680
adityachopra29 <adityachopra2912@gmail.com>
2023-05-21 19:30:38 +0530

    first commit

---
+++ sign-in(then)/index.js
@@ -0,0 +1,31 @@
+document.getElementById("submit").addEventListener("click",function(e){
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+
+    e.preventDefault();
+
+    var obj={
+    "email":email,
+    "password":password
+    }
+
+    let error_box = document.getElementById("error-box");
+
+    function displayError(){
+        let error = document.createElement("p");
+        let error_node = document.createTextNode("Data cant be received");
+        error.appendChild(error_node);
+        error_box.appendChild(error);
+    }
+
+    fetch('https://reqres.in/api/login', {
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch(error => displayError());
+})
